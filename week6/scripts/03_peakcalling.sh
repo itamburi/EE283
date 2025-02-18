c651020e3c7a86900318fa49517d4476d1a14f78
itamburi <itamburi@uci.edu>
2025-02-17 16:31:26 -0800

    made prob3 script

--- week6/scripts/03_peakcalling.sh
+++ week6/scripts/03_peakcalling.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 #SBATCH -A CLASS-ECOEVO283
 #SBATCH --job-name=call_peaks    # Job name 
-#SBATCH --cpus-per-task 8
-#SBATCH --array=1-4 
-#SBATCH --mem-per-cpu=8G
+#SBATCH --cpus-per-task 32
+#SBATCH --mem-per-cpu=3G
 #SBATCH --output=/dfs6/pub/itamburi/ee283/week6/log/%x_%j.out   # log based on jobname
 #SBATCH --error=/dfs6/pub/itamburi/ee283/week6/log/%x_%j.err    # error based on jobname
 
@@ -12,61 +11,54 @@ module load samtools/1.15.1
 module load bedtools2/2.30.0
 module load ucsc-tools/v429
 
+module load miniconda3/24.9.2
+source /opt/apps/miniconda3/24.9.2/etc/profile.d/conda.sh
+conda activate macs2
+
+ref="/dfs6/pub/itamburi/ee283/datalinks/refs"
 wd="/dfs6/pub/itamburi/ee283/week6"
+bam="/dfs6/pub/itamburi/ee283/week6/bam.processed"
+out="/dfs6/pub/itamburi/ee283/week6/pkfiles"
 samples="/dfs6/pub/itamburi/ee283/week6/atac.samples.txt"
 
-prefix=`head -n $SLURM_ARRAY_TASK_ID ${samples} | tail -n 1`
 
+samtools merge -f -o ${out}/A4ED_merged.bam \
+	${bam}/P004_A4_ED_2.chrX.dd.bam \
+	${bam}/P005_A4_ED_3.chrX.dd.bam \
+	${bam}/P006_A4_ED_4.chrX.dd.bam
 
-samtools merge -o condition1.bam sample1.bam sample2.bam sample3.bam
-
-bedtools bamtobed -i condition1.bam | \
+bedtools bamtobed -i ${out}/A4ED_merged.bam | \
 	awk -F $'\t' 'BEGIN {OFS = FS}{ if ($6 == "+") {$2 = $2 + 4} else if ($6 == "-") {$3 = $3 - 5} print $0}' \
-	> condition1.tn5.bed
+	> ${out}/A4ED_merged.tn5.bed
 
-macs2 callpeak -t condition1.tn5.bed \
-	-n condition1 \
+macs2 callpeak -t ${out}/A4ED_merged.tn5.bed \
+	--outdir ${out} \
+	-n A4ED \
 	-f BED -g mm -q 0.01 --nomodel \
 	--shift -75 --extsize 150 \
-	--call-summits --keep-dup all -B --broad
+	--broad --keep-dup all --bdg
+
+
 
 # I cd into my scripts folder and download a function ucsctools doesn't have.
 # Run in interactive session:
-# rsync -aP rsync://hgdownload.soe.ucsc.edu/genome/admin/exe/linux.x86_64.v369/fetchChromSizes 
+# rsync -aP rsync://hgdownload.soe.ucsc.edu/genome/admin/exe/linux.x86_64.v369/fetchChromSizes . 
+# ${wd}/scripts/fetchChromSizes ${ref}/dm6.fasta > ${ref}/dm6.chrom.sizes
+# ... this command didnt seem to work. So isntead I downloaded the chrom sizes file from UCSC and reformmated by removing the chr:
+#wget -O - http://hgdownload.soe.ucsc.edu/goldenPath/dm6/bigZips/dm6.chrom.sizes | sed 's/^chr//' > dm6.chrom.sizes.ucsc
 
-${wd}/scripts/fetchChromSizes ${ref}/dm6 > ${ref}/dm6.chrom.sizes
+LC_COLLATE=C sort -k1,1 -k2,2n ${out}/A4ED_treat_pileup.bdg > ${out}/A4ED_treat_pileup.sorted.bdg
 
-LC_COLLATE=C sort -k1,1 -k2,2n ${wd}/bdg.files/broad_treat_pileup.bdg > ${wd}/bdg.files/broad_treat_pileup.sorted.bdg
+# awk 'NR==FNR {chr_size[$1]=$2; next} $3 > chr_size[$1]' ${ref}/dm6.chrom.sizes.ucsc ${out}/A4ED_treat_pileup.sorted.bdg > ${out}/A4ED_treat_pileup.safeends.bdg
+# ... this didnt work, probably because the .sorted.bdg only contains X chromosome. instead use:
+awk 'NR==FNR {if ($1 == "X") chr_size[$1]=$2; next} $1 == "X" && $3 <= chr_size[$1]' ${ref}/dm6.chrom.sizes.ucsc ${out}/A4ED_treat_pileup.sorted.bdg > ${out}/A4ED_treat_pileup.safeends.bdg 
 
-awk 'NR==FNR {chr_size[$1]=$2; next} $3 > chr_size[$1]' ${ref}/dm6.chrom.sizes ${wd}/bdg.files/broad_treat_pileup.sorted.bdg > ${wd}/bdg.files/broad_treat_pileup.safeends.bdg
-
-bedGraphToBigWig broad_treat_pileup.safeends.bdg ref/dm6.chrom_sizes broad_peaks.bw
-
+#awk '{print "chr" $1 "\t" $2}' ${ref}/dm6.chrom.sizes.ucsc > ${ref}/dm6_with_chr.chrom.sizes.ucsc
+#awk '{if ($1 ~ /^[0-9]+$/) $1 = "chr" $1; else if ($1 !~ /^chr/) $1 = "chr" $1; print $0}' ${out}/A4ED_treat_pileup.safeends.bdg > ${out}/A4ED_treat_pileup_with_chr.bdg
 
 
-###########
-module load java/1.8.0
-module load samtools/1.15.1
-module load picard-tools/2.27.1
-
-samples="/dfs6/pub/itamburi/ee283/week6/atac.samples.txt"
-atacbam="/dfs6/pub/itamburi/ee283/week3/prob4/ATACbam"
-bamout="/dfs6/pub/itamburi/ee283/week6/bam.processed"
-
-prefix=`head -n $SLURM_ARRAY_TASK_ID ${samples} | tail -n 1`
-
-# need indexed file to view chr X in the next step
-samtools index -c ${atacbam}/${prefix}.sort.bam
-
-samtools view -q 30 -b ${atacbam}/${prefix}.sort.bam X | \
-	samtools sort -O BAM -o ${bamout}/${prefix}.chrX.sort.bam #probably dont need to sort again, but just in case...
-
-java -jar /opt/apps/picard-tools/2.27.1/picard.jar MarkDuplicates \
-	I=${bamout}/${prefix}.chrX.sort.bam \
-	O=${bamout}/${prefix}.chrX.dd.bam \
-	M=${bamout}/${prefix}.chrX.dd.metrics \
-	REMOVE_DUPLICATES=true
-
+bedGraphToBigWig ${out}/A4ED_treat_pileup_with_chr.bdg ${ref}/dm6_with_chr.chrom.sizes.ucsc ${out}/A4ED_broad_peaks_with_chr.bw
+#bedGraphToBigWig ${out}/A4ED_treat_pileup.safeends.bdg ${ref}/dm6.chrom.sizes.ucsc ${out}/A4ED_broad_peaks.bw
 
 
 
